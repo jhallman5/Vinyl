f5708ae1204629c8ce7f8b035f398e9c1c1944f4
John <jhallman5@gmail.com>
2017-08-14 13:48:31 -0700

    Basic home page completion

--- src/models/reviews.js
+++ src/models/reviews.js
@@ -1,8 +1,12 @@
 const Review = require('./db/reviews')
+const moment = require('moment')
 
 const findThreeMostRecent = () =>
   Review.getAll()
-    .then(reviews => reviews.filter((review, index) => index < 3))
+    .then(reviews => {
+      reviews.forEach(review => review.created_on = moment(review.created_on).format("MMM Do YY"))
+      return reviews.filter((review, index) => index < 3)
+    })
     .catch(error => {
       console.log('Error in model')
       throw error
