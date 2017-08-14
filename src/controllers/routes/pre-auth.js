4ee4c3ec6fc00d51ab2bd74e73184fc666671104
John <jhallman5@gmail.com>
2017-08-14 11:41:24 -0700

    Adds home splash image

--- src/controllers/routes/pre-auth.js
+++ src/controllers/routes/pre-auth.js
@@ -3,7 +3,7 @@ const Album = require('../../models/albums')
 
 router.get('/', (req, res) => {
   Album.getAll()
-    .then(albums => res.render('index', {albums}))
+    .then(albums => res.render('home', {albums}))
     .catch(error => res.status(500).render('error', {error}))
 })
 
