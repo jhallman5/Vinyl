a3f2fa69f8aeefd4b1461f3144fdd604171511c9
John <jhallman5@gmail.com>
2017-08-14 12:25:12 -0700

    Adds review card and review quiuries for homepage

--- src/models/db/reviews.js
+++ src/models/db/reviews.js
@@ -3,6 +3,9 @@ const knex = require('knex')(require('./knexfile'))
 const getAll = () =>
   knex.select('*')
     .from('reviews')
+    .leftOuterJoin('users', 'reviews.user_id', 'users.id')
+    .leftOuterJoin('albums', 'reviews.album_id', 'albums.id')
+    .orderBy('created_on', 'desc')
 
 module.exports = {
   getAll
--- src/models/reviews.js
+++ src/models/reviews.js
@@ -3,6 +3,10 @@ const Review = require('./db/reviews')
 const findThreeMostRecent = () =>
   Review.getAll()
     .then(reviews => reviews.filter((review, index) => index < 3))
+    .catch(error => {
+      console.log('Error in model')
+      throw error
+    })
 
 module.exports = {
   findThreeMostRecent,
