7c4c822f80919a75c83b5806a32cc4b414c9157a
John <jhallman5@gmail.com>
2017-08-15 12:01:48 -0700

    Adds event listener to delete card

--- src/public/script.js
+++ src/public/script.js
@@ -1,1 +1,13 @@
-console.log('hello from the browser JavaScript')
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.owned-card').forEach(card => {
+    card.addEventListener('click', function(event) {
+      confirm("Are you sure you want to delete this review?")
+        ? null
+        : event.preventDefault()
+    })
+  })
+
+
+
+
+})
