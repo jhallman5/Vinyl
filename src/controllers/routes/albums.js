2521a639b768109a6739b3338d6debccd3df72b9
John <jhallman5@gmail.com>
2017-08-15 14:48:46 -0700

    Adds review page and finishes album page

--- src/controllers/routes/albums.js
+++ src/controllers/routes/albums.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const Album = require('../../models/albums')
 
+
 router.get('/:albumID', (req, res) => {
   Album.findById(req.params.albumID)
     .then(album => res.render('album', {album, session: req.session.passport}))
